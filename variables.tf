3c9dd7106d9af01bd25ff43b410abad04e18a678
venkyredd <146801831+venkyredd@users.noreply.github.com>
2025-05-05 14:12:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,16 +1,19 @@
 variable "aws_region" {
   type        = string
-  description = "us-east-1"
+  description = "AWS region for all resources"
+  default     = "us-east-1"
 }
 
 variable "state_bucket_name" {
   type        = string
-  description = "v-backend-s3"
+  description = "Name of existing S3 bucket to store Terraform state"
+  default     = "v-backend-s3"
 }
 
 variable "lock_table_name" {
   type        = string
-  description = "v-backend-db"
+  description = "Name of existing DynamoDB table for state locking"
+  default     = "v-backend-db"
 }
 
 variable "vpc_cidr" {
@@ -37,3 +40,38 @@ variable "container_port" {
   default     = 80
 }
 
+variable "alb_sg_id" {
+  type        = string
+  description = "Security group ID for the ALB"
+}
+
+variable "ecs_sg_id" {
+  type        = string
+  description = "Security group ID for ECS service"
+}
+
+variable "execution_role" {
+  type        = string
+  description = "IAM execution role ARN for ECS tasks"
+}
+
+variable "log_group_name" {
+  type        = string
+  description = "Name of CloudWatch log group for ECS"
+}
+
+variable "subnet_ids" {
+  type        = list(string)
+  description = "List of subnet IDs for ECS tasks and ALB"
+}
+
+variable "vpc_id" {
+  type        = string
+  description = "VPC ID used by ECS and ALB"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "Name of the ECS cluster"
+  default     = "my-app-cluster"
+}
